7c88ef90225b3be6d8ed4a525b3d236f7f64aed7
Karl Benjamin Bughaw <70707230+Golgrax@users.noreply.github.com>
2024-11-22 10:30:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const gokuImage = document.getElementById('goku-image');
-const body = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+  const gokuImage = document.getElementById('goku-image');
+  const body = document.body;
 
-gokuImage.addEventListener('click', () => {
-  if (gokuImage.src.includes('goku normal (1).png')) {
-    gokuImage.src = 'goku god (1).png';
-    body.style.backgroundImage = "url('aura.gif')";
-  } else {
-    gokuImage.src = 'goku normal (1).png';
-    body.style.backgroundImage = '';
-  }
+  gokuImage.addEventListener('click', () => {
+    if (gokuImage.src.includes('goku normal (1).png')) {
+      gokuImage.src = '/goku god (1).png';
+      body.style.backgroundImage = "url('/aura.gif')";
+    } else {
+      gokuImage.src = '/goku normal (1).png';
+      body.style.backgroundImage = '';
+    }
+  });
 });
